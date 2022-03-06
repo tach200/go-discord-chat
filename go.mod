c4056cb4a2662dcc4dd177debdfca60ab1d3c759
Thomas Acheson <t.acheson@reply.com>
2022-03-06 15:36:47 +0000

    significant changes

--- go.mod
+++ go.mod
@@ -14,16 +14,29 @@ require (
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
 )
 
+require github.com/spf13/viper v1.10.1
+
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -16,44 +17,37 @@ import (
 	pb "github.com/achmang/go-discord-chat/proto"
 	"github.com/bwmarrin/discordgo"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/spf13/viper"
 )
 
 var (
 	Token     string
 	ChannelID string
+	GrpcPort  string
+	RestPort  string
 )
 
-func init() {
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.StringVar(&ChannelID, "c", "942196758115659817", "Channel ID")
+// restServer starts the http rest server to listen for incoming messages,
+// pass in a discrod session so that incoming message can be sent to discord channel.
+func restServer(discord *discordgo.Session, port string) {
+	mux := runtime.NewServeMux()
 
-	flag.Parse()
+	pb.RegisterDiscordMessageHandlerServer(context.Background(), mux, &handlers.DiscordBotServer{
+		UnimplementedDiscordMessageServer: pb.UnimplementedDiscordMessageServer{},
+		Session:                           discord,
+		ChannelID:                         ChannelID,
+	})
+
+	addr := fmt.Sprintf("localhost:%s", port)
+	http.ListenAndServe(addr, mux)
 }
 
-func main() {
-	// Create the Discord Session
-	discord, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		log.Fatalf("could not create new discord session: %v", err)
-	}
-	log.Println("Discord session created")
+// grpcServer starts the grpc server to listen for incoming messages,
+// pass in a discrod session so that incoming message can be sent to discord channel.
+func grpcServer(discord *discordgo.Session, port string) {
+	addr := fmt.Sprintf("localhost:%s", port)
 
-	go func() {
-		// mux
-		mux := runtime.NewServeMux()
-		// register
-		pb.RegisterDiscordMessageHandlerServer(context.Background(), mux, &handlers.DiscordBotServer{
-			UnimplementedDiscordMessageServer: pb.UnimplementedDiscordMessageServer{},
-			Session:                           discord,
-			ChannelID:                         ChannelID,
-		})
-		// launch http
-		http.ListenAndServe("localhost:1133", mux)
-	}()
-
-	//Start gRPC server
-	// TODO set flag here for port
-	lis, err := net.Listen("tcp", "localhost:1122")
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -68,6 +62,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("grpc sever couldnt sevre %v", err)
 	}
+}
+
+func init() {
+	viper.SetConfigName("env")
+	viper.AddConfigPath("/configs/")
+	viper.AddConfigPath("/configs/secrets")
+	viper.AddConfigPath("$HOME/.configs")
+	viper.AddConfigPath(".")
+	viper.SetConfigFile("yml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("error: couldn't read config file: %s", err)
+	}
+
+	flag.StringVar(&Token, "t", viper.GetString("token"), "Bot Token")
+	flag.StringVar(&ChannelID, "c", viper.GetString("channelID"), "Channel ID")
+	flag.StringVar(&GrpcPort, "gp", viper.GetString("grpcPort"), "gRPC Port")
+	flag.StringVar(&RestPort, "rp", viper.GetString("restPort"), "REST Port")
+
+	flag.Parse()
+}
+
+func main() {
+	// Create the Discord Session
+	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatalf("could not create new discord session: %v", err)
+	}
+	log.Println("Discord session created")
+
+	go restServer(discord, RestPort)
+	go grpcServer(discord, GrpcPort)
 
 	// Shut the server down properly
 	sc := make(chan os.Signal, 1)
